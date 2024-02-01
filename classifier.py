d5d1cb67867d4675fd79985d77fbfa2dc7f9407c
Nagakburos <103370604+Nagakburos@users.noreply.github.com>
2024-02-01 19:24:16 -0300

    final

--- classifier.py
+++ classifier.py
@@ -3,26 +3,39 @@ from tensorflow.keras import models
 from PIL import Image
 import numpy as np
 
+class_names = {
+    0: 'Avião',
+    1: 'Automovel',
+    2: 'Passaro',
+    3: 'Gato',
+    4: 'Veado',
+    5: 'Cachorro',
+    6: 'Sapo',
+    7: 'Cavalo',
+    8: 'Ovelha',
+    9: 'Caminhão',
+}
 
 model = models.load_model("baseline.keras")
 
 def predict_image(model, path_to_img):
-    img + Image.open(path_to_img)
+    img = Image.open(path_to_img)
     img = img.convert("RGB")
     img = img.resize((32,32))
-    data = np.asarry(img)
-    print ("before", data[0][0])
+    data = np.asarray(img)
     data = data /255
-    print ("after", data[0][0])
     probs = model.predict(np.array([data])[:1])
-    print(probs)
-    print(probs.max())
-    print(np.argmax(probs))
 
+    top_prob = probs.max()
+    top_pred = class_names[np.argmax(probs)]
+
+    return top_prob, top_pred
 
 
 content = ""
 img_path = "placeholder_image.png" ##adiciona Logo
+prob = 0
+pred = ""
 
 index = """ 
 <|text-center|
@@ -31,8 +44,11 @@ index = """
 <|{content}|file_selector|extensions=.png|>
 Selecione uma imagem do seu armazenamento
 
+<|{pred}|>
+
 <|{img_path}|image|>
-<|label here|indicator|value=0|min=0|max=100|width=25vw|>
+
+<|Minha Confiança {prob}|indicator|value={prob}|min=0|max=100|width=25vw|>
 
 >
 """
@@ -40,8 +56,11 @@ Selecione uma imagem do seu armazenamento
 
 def on_change(state, var_name, var_val):
     if var_name == "content":
+        top_prob, top_pred = predict_image(model, var_val)
+        state.prob = round(top_prob * 100)
+        state.pred =  "isso é um(a) " + top_pred
         state.img_path = var_val
-        predict_image(model, var_val)
+
 
 app = Gui (page=index)
 
